9ba7655ed9cad12d3a5ecaf018403ea4226bbc95
Thanh Ky <huynhtanngoc91@gmail.com>
2024-12-24 15:19:24 +0700

    update

--- test/contracts/proxy/DmtpProxy.sol
+++ test/contracts/proxy/DmtpProxy.sol
@@ -55,7 +55,6 @@ contract DmtpProxy is ERC1967Proxy {
     // This is acceptable if the admin is always a ProxyAdmin instance or similar contract
     // with its own ability to transfer the permissions to another account.
     address private _admin;
-    address public _initialOwner;
 
     /**
      * @dev The proxy caller is the current admin, and can't fallback to the proxy target.
@@ -69,12 +68,11 @@ contract DmtpProxy is ERC1967Proxy {
      */
     constructor(
         address _logic,
-        address initialOwner
+        address admin_
     ) payable ERC1967Proxy(_logic, bytes("")) {
-        _admin = address(new DmtpProxyAdmin(initialOwner));
+        _admin = admin_;
         // Set the storage value and emit an event for ERC-1967 compatibility
         ERC1967Utils.changeAdmin(_proxyAdmin());
-        _initialOwner = initialOwner;
     }
 
     /**
@@ -82,19 +80,6 @@ contract DmtpProxy is ERC1967Proxy {
      */
     function _proxyAdmin() internal view virtual returns (address) {
         return _admin;
-    }
-
-    function changeAdmin(address newAdmin) public {
-        // require(
-        //     _initialOwner == msg.sender,
-        //     ERC1967Utils.ERC1967InvalidAdmin(msg.sender)
-        // );
-        _admin = newAdmin;
-        ERC1967Utils.changeAdmin(newAdmin);
-    }
-
-    function getAdmin() public view returns (address) {
-        return _proxyAdmin();
     }
 
     /**
@@ -129,7 +114,4 @@ contract DmtpProxy is ERC1967Proxy {
         );
         ERC1967Utils.upgradeToAndCall(newImplementation, data);
     }
-
-    // Receive function to accept ether
-    receive() external payable {}
 }
--- test/contracts/proxy/DmtpProxyAdmin.sol
+++ test/contracts/proxy/DmtpProxyAdmin.sol
@@ -6,11 +6,4 @@ import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 contract DmtpProxyAdmin is ProxyAdmin {
     constructor(address initialOwner) ProxyAdmin(initialOwner) {}
-
-    function isAdminOf(address proxy) external view returns (bool) {
-        // We need to manually run the static call since the getter cannot be flagged as view
-        // bytes4(keccak256("implementation()")) == 0x5c60da1b
-        (bool success, ) = address(proxy).staticcall(hex"5c60da1b");
-        return success;
-    }
 }
